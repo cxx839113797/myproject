5e5c51077072d5f4097f63c1ccf2b72cfd2f1c82
cxx839113797 <839113797@qq.com>
2019-05-10 02:35:35 +0800

    update common

--- common/suds_services.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-# @Time  : 2019/5/9 23:10
-# @Author: xiaoxiao
-# @File  : suds_services.py
-
-from suds.client import Client
-
---
+++ report/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# @Time  : 2019/5/9 22:00
+# @Author: xiaoxiao
+# @File  : __init__.py.py
