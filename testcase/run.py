ec2efc523e092b2220e1bd0d7c9ef06906fe7516
cxx839113797 <839113797@qq.com>
2019-05-10 06:06:58 +0800

    update run.py

--- testcase/run.py
+++ testcase/run.py
@@ -4,14 +4,14 @@
 # @File  : run.py
 
 import unittest
-from common.contants import report_dir
+from common import contants
 import HTMLTestRunnerNew
-from common.contants import case_dir
-
-case=unittest.defaultTestLoader.discover(case_dir,"test_*.py")
 
 
-with open(report_dir+"\WebService.html","wb") as f:
+case=unittest.defaultTestLoader.discover(contants.case_dir,"test_*.py")
+
+
+with open(contants.report_dir+"\WebService.html","wb") as f:
     runner=HTMLTestRunnerNew.HTMLTestRunner\
         ( stream=f, verbosity=2,title="这是WebService的测试报告",description="测试WebService接口",tester="xiaoxiao")
     runner.run(case)
